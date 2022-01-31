c65477a0134c6bb91fc1613031781508de95d49b
Yuvasri <yuvasri207@gmail.com>
2022-01-31 23:26:19 +0530

    Add files via upload

---
+++ FRACTION OF A DECIMAL VALUE.py
@@ -0,0 +1,13 @@
+arr=[-4, 3, -9, 0, 4, 1]
+length = len(arr)
+positivecount = 3
+negativecount = 2
+zerocount = 1
+for i in range(length):
+    print("%1.6f" %(positivecount / length))
+    print("%1.6f" %(negativecount / length))
+    print("%1.6f" %(zerocount / length))
+    print()
+
+
+
